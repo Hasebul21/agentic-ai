5b6e3b1e528dcd37fa2f7a6914076155faa3b43e
Hasebul Hassan Chowdhury <hasebulhassan21@gmail.com>
2026-06-30 19:28:31 +0600

    added tools

--- tools.py
+++ tools.py
@@ -3,13 +3,21 @@ from db.raw import run_sql
 
 def get_all_inventory():
     rows = run_sql(
-        "SELECT id, quantity FROM inventory"
+        "SELECT id, quantity FROM inventory LIMIT 5"
+    )
+    return rows
+
+def get_inventory_by_product_id(product_id: int):
+    rows = run_sql(
+        "SELECT id, quantity FROM inventory WHERE product_id = :product_id",
+        {"product_id": 11}
+        
     )
     return rows
 
 
-
-
 if __name__ == "__main__":
-    get_all_inventory()
-    
+    inventory = get_all_inventory()
+    print(inventory)
+    product_ = get_inventory_by_product_id(1)
+    print(product_)
