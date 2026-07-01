35585d20ee82610264421c9a47af56f0c20d9343
Hasebul Hassan Chowdhury <hasebulhassan21@gmail.com>
2026-07-01 14:38:12 +0600

    added tools

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-from openai import OpenAI
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from tools import tools
 
 load_dotenv()
 
-client = OpenAI()
+class Answer(BaseModel):
+    summary: str
+    results: list
+    tools_used: list
+    
+user_prompt_content = """
+    can you give me the name of 
+    the customers who are from Bangladesh and USA
+    i just need name
+"""
 
+system_prompt_content = """
+    You are a database manager assistant for a company.
+    You will use accurate tools to query the database 
+    and provide the user valid answer what he asked for.
+    Dont Hellucinate or make up any answer. If you dont know the answer, 
+    say "I dont know".
+"""
 
-def invoke(user_message: str) -> str:
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{"role": "user", "content": user_message}]
-    )
-    ai_response = response.choices[0].message.content
+llm = ChatOpenAI(model="gpt-4o-mini")
+prompt = {
+    "messages": [
+        {
+            "role": "user", 
+            "content": user_prompt_content
+        }
+      ]
+    }
 
-    return ai_response
+agent = create_agent(
+    model="gpt-4o-mini",
+    tools=tools,
+    system_prompt=system_prompt_content,
+    response_format=Answer,
+)
 
-
-if __name__ == "__main__":
-    from db.raw import run_sql
-
-    rows = run_sql(
-        "SELECT id, quantity FROM inventory"
-    )
-    for row in rows:
-        print(row.id, row.quantity)
+response = agent.invoke(prompt)
+print(response["structured_response"])
--- tools.py
+++ tools.py
@@ -1,23 +1,54 @@
 from db.raw import run_sql
+from langchain_core.tools import Tool
 
-
-def get_all_inventory():
-    rows = run_sql(
-        "SELECT id, quantity FROM inventory LIMIT 5"
-    )
+def get_all_inventory(_: str = "") -> list:
+    rows = run_sql("""
+                   SELECT name, quantity FROM inventory
+                   JOIN products ON inventory.product_id = products.id
+                   WHERE products.is_active = true
+                   ORDER BY quantity desc
+                   LIMIT 5""")
     return rows
+
 
 def get_inventory_by_product_id(product_id: int):
     rows = run_sql(
         "SELECT id, quantity FROM inventory WHERE product_id = :product_id",
-        {"product_id": 11}
-        
+        {"product_id": 11},
     )
     return rows
 
 
+def get_product_by_price_and_status(price: int, isActive: bool) -> list:
+    rows = run_sql(
+        """ SELECT * FROM products WHERE 
+        price < :price AND is_active =:isActive
+        ORDER BY price desc """,
+        {"price": price, "isActive": isActive},
+    )
+    return rows
+
+
+def get_customer_by_country(country: str):
+    rows = run_sql(
+        "SELECT * FROM customers WHERE country =:country", {"country": country}
+    )
+    return rows
+
+top_inventory_tool = Tool(
+    name="get_top_inventory",
+    func=get_all_inventory,
+    description="Get the top 5 items from the inventory with the highest quantity.",
+)
+
+find_customer_by_country_tool = Tool(
+    name="find_customer_by_country",
+    func=get_customer_by_country,
+    description="Get all customers from a specific country.",
+)
+
+tools = [top_inventory_tool, find_customer_by_country_tool]
+
 if __name__ == "__main__":
-    inventory = get_all_inventory()
-    print(inventory)
-    product_ = get_inventory_by_product_id(1)
-    print(product_)
+    res = get_all_inventory()
+    print(res)
